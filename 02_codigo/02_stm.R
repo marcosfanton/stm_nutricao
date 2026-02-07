be583f9954e07865e1eb581bc8c4c7fca22a1c4a
marcosfanton <marcos.fanton@ufsm.com>
2026-02-07 09:59:16 -0300

    modificação de arquivo de limpeza (tudo em 02_limpeza.R)

--- 02_codigo/02_stm.R
+++ 02_codigo/02_stm.R
@@ -3,14 +3,10 @@
 # Abrir dados
 dados <- readRDS(file = "01_dados/catalogo_limpo.RDS")
 
-# Filtragem de observações com DS_RESUMO < 10 (n: 5346)
-dados <- dados |>
-  dplyr::filter_out(
-    stringi::stri_count_words(catalogo_raw$DS_RESUMO) < 10
-  ) # n: 67
-
 # Inserção em ID para cada observação/documento
 dados <- dados |>
-  dplyr::mutate(DOC_ID = row_number())
 
-# NGRAMS e STOPWORDS
+  # NGRAMS e STOPWORDS
+
+  teste <- dados |>
+  filter(stringi::stri_count_words(catalogo_raw$DS_RESUMO) < 20)
