1d08322e4e528d4c3fe97ce53ece13c5c386281c
marcosfanton <marcos.fanton@ufsm.br>
2026-03-09 11:23:41 -0300

    bi- e trigrams. Tentando organizar os td-idf.

--- 02_codigo/00_testes.R
+++ 02_codigo/00_testes.R
@@ -117,3 +117,78 @@ limpeza_texto <- function(
       )
     )
 }
+
+
+
+# Contagem de tokens totais
+N_unigram <- sum(unigrams$n_uni)
+
+# Bigrams
+bigrams <- dados |>
+  tidytext::unnest_tokens(bigram, DS_RESUMO, "ngrams", n = 2) |> # Formação da variável bigram com todas palavras do resumo
+  tidyr::separate(bigram, into = c("word1", "word2"), sep = " ") |> # Separação dos bigrams para remoção de stopwords
+  dplyr::count(word1, word2, sort = TRUE)
+
+pmi_bigrams <- bigrams |>
+  filter(n >= 10) |>
+  left_join(unigrams, by = c("word1" = "word")) |>
+  left_join(unigrams, by = c("word2" = "word"), suffix = c("_w1", "_w2")) |>
+  mutate(
+    pmi = log2(
+      (n / N_unigram) / ((n_uni_w1 / N_unigram) * (n_uni_w2 / N_unigram))
+    )
+  ) |>
+  filter(pmi >= 5) |>
+  arrange(desc(pmi))
+
+# Trigrams
+tridados <- dados |>
+  select(DOC_ID, DS_RESUMO) |>
+  tidytext::unnest_tokens(output = trigram, DS_RESUMO, token = "ngrams", n = 3) # Formação da variável bigram com todas palavras do resumo
+trigrams_sep <- tridados |>
+  tidyr::separate(trigram, into = c("word1", "word2", "word3"), sep = " ") # Separação dos bigrams para remoção de stopwords
+trigrams <- trigrams_sep |>
+  tidyr::unite("trigram", c(word1, word2, word3), sep = " ") |> # Unificação dos bigrams novamente
+  dplyr::count(trigram, sort = TRUE) |> # Contagem da frequência absoluta de cada bigram
+  dplyr::filter(n >= 100) # Trigram com (ou mais de) 25 ocorrências
+
+# Tetragrams
+n_dados <- dados |>
+  select(DOC_ID, DS_RESUMO) |>
+  tidytext::unnest_tokens(
+    output = n_gram,
+    DS_RESUMO,
+    token = "ngrams",
+    n = 4
+  ) # Formação da variável bigram com todas palavras do resumo
+n_grams_sep <- n_dados |>
+  tidyr::separate(
+    n_gram,
+    into = c("word1", "word2", "word3", "word4"),
+    sep = " "
+  ) # Separação dos bigrams para remoção de stopwords
+n_grams <- n_grams_sep |>
+  tidyr::unite("n_gram", c(word1, word2, word3, word4), sep = " ") |> # Unificação dos bigrams novamente
+  dplyr::count(n_gram, sort = TRUE) |> # Contagem da frequência absoluta de cada bigram
+  dplyr::filter(n >= 10) # Tetragram com (ou mais de) 10 ocorrências
+
+
+# Tetragrams
+n_dados <- dados |>
+  select(DOC_ID, DS_RESUMO) |>
+  tidytext::unnest_tokens(
+    output = n_gram,
+    DS_RESUMO,
+    token = "ngrams",
+    n = 5
+  ) # Formação da variável bigram com todas palavras do resumo
+n_grams_sep <- n_dados |>
+  tidyr::separate(
+    n_gram,
+    into = c("word1", "word2", "word3", "word4", "word5"),
+    sep = " "
+  ) # Separação dos bigrams para remoção de stopwords
+n_grams <- n_grams_sep |>
+  tidyr::unite("n_gram", c(word1, word2, word3, word4, word5), sep = " ") |> # Unificação dos bigrams novamente
+  dplyr::count(n_gram, sort = TRUE) |> # Contagem da frequência absoluta de cada bigram
+  dplyr::filter(n >= 20)
--- 02_codigo/02_nutrigrams.R
+++ 02_codigo/02_nutrigrams.R
@@ -2,10 +2,10 @@
 library(tidyverse)
 library(here)
 library(tidytext)
+library(stopwords)
 
 # Abrir dados (n: 5.284)
 dados <- readRDS(file = "01_dados/catalogo_limpo.RDS")
-
 
 # Função de limpeza de texto ####
 limpeza_texto <- function(dados, variavel, idioma = "pt") {
@@ -39,6 +39,10 @@ trigrams <- dados |>
   dplyr::filter(n >= 50) |>
   tidyr::unite(trigram, word1, word2, word3, sep = "_")
 
+# Salvar arquivo com trigrams
+trigrams |>
+  readr::write_csv("01_dados/trigrams.csv")
+
 # Bigrams
 bigrams <- dados |>
   tidytext::unnest_tokens(bigram, DS_RESUMO, token = "ngrams", n = 2) |>
@@ -47,76 +51,17 @@ bigrams <- dados |>
   dplyr::filter(n >= 50) |>
   tidyr::unite(bigram, word1, word2, sep = "_")
 
+# Salvar arquivo com bigrams
+bigrams |>
+  readr::write_csv("01_dados/bigrams.csv")
 
-# Contagem de tokens totais
-N_unigram <- sum(unigrams$n_uni)
-
-# Bigrams
-bigrams <- dados |>
-  tidytext::unnest_tokens(bigram, DS_RESUMO, "ngrams", n = 2) |> # Formação da variável bigram com todas palavras do resumo
-  tidyr::separate(bigram, into = c("word1", "word2"), sep = " ") |> # Separação dos bigrams para remoção de stopwords
-  dplyr::count(word1, word2, sort = TRUE)
-
-pmi_bigrams <- bigrams |>
-  filter(n >= 10) |>
-  left_join(unigrams, by = c("word1" = "word")) |>
-  left_join(unigrams, by = c("word2" = "word"), suffix = c("_w1", "_w2")) |>
-  mutate(
-    pmi = log2(
-      (n / N_unigram) / ((n_uni_w1 / N_unigram) * (n_uni_w2 / N_unigram))
-    )
+# Análise de expressões acadêmicas por TD-IDF #### 
+tf_idf <- dados |>
+  tidytext::unnest_tokens(word, DS_RESUMO) |>
+  dplyr::filter_out(
+    stringr::str_detect(word, "^[0-9]+$"),
+    stringr::str_detect(word, "^[a-záéíóúâêôãõç]{3,}$")
   ) |>
-  filter(pmi >= 5) |>
-  arrange(desc(pmi))
-
-# Trigrams
-tridados <- dados |>
-  select(DOC_ID, DS_RESUMO) |>
-  tidytext::unnest_tokens(output = trigram, DS_RESUMO, token = "ngrams", n = 3) # Formação da variável bigram com todas palavras do resumo
-trigrams_sep <- tridados |>
-  tidyr::separate(trigram, into = c("word1", "word2", "word3"), sep = " ") # Separação dos bigrams para remoção de stopwords
-trigrams <- trigrams_sep |>
-  tidyr::unite("trigram", c(word1, word2, word3), sep = " ") |> # Unificação dos bigrams novamente
-  dplyr::count(trigram, sort = TRUE) |> # Contagem da frequência absoluta de cada bigram
-  dplyr::filter(n >= 100) # Trigram com (ou mais de) 25 ocorrências
-
-# Tetragrams
-n_dados <- dados |>
-  select(DOC_ID, DS_RESUMO) |>
-  tidytext::unnest_tokens(
-    output = n_gram,
-    DS_RESUMO,
-    token = "ngrams",
-    n = 4
-  ) # Formação da variável bigram com todas palavras do resumo
-n_grams_sep <- n_dados |>
-  tidyr::separate(
-    n_gram,
-    into = c("word1", "word2", "word3", "word4"),
-    sep = " "
-  ) # Separação dos bigrams para remoção de stopwords
-n_grams <- n_grams_sep |>
-  tidyr::unite("n_gram", c(word1, word2, word3, word4), sep = " ") |> # Unificação dos bigrams novamente
-  dplyr::count(n_gram, sort = TRUE) |> # Contagem da frequência absoluta de cada bigram
-  dplyr::filter(n >= 10) # Tetragram com (ou mais de) 10 ocorrências
-
-
-# Tetragrams
-n_dados <- dados |>
-  select(DOC_ID, DS_RESUMO) |>
-  tidytext::unnest_tokens(
-    output = n_gram,
-    DS_RESUMO,
-    token = "ngrams",
-    n = 5
-  ) # Formação da variável bigram com todas palavras do resumo
-n_grams_sep <- n_dados |>
-  tidyr::separate(
-    n_gram,
-    into = c("word1", "word2", "word3", "word4", "word5"),
-    sep = " "
-  ) # Separação dos bigrams para remoção de stopwords
-n_grams <- n_grams_sep |>
-  tidyr::unite("n_gram", c(word1, word2, word3, word4, word5), sep = " ") |> # Unificação dos bigrams novamente
-  dplyr::count(n_gram, sort = TRUE) |> # Contagem da frequência absoluta de cada bigram
-  dplyr::filter(n >= 20)
+  dplyr::count(DOC_ID, word, sort = TRUE) |>
+  tidytext::bind_tf_idf(word, DOC_ID, n) |>
+  dplyr::arrange(desc(tf_idf))
